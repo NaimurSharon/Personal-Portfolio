c214e816f784b68ffd79364550ed9344fb334015
Naimur Sharon <100515357+NaimurSharon@users.noreply.github.com>
2024-11-28 22:11:13 +0600

    Update BottomHeading.js

--- src/components/BottomHeading.js
+++ src/components/BottomHeading.js
@@ -6,7 +6,13 @@ export default function BottomHeading() {
   return (
     <div className="bottomHeading">
       <div className="bounding">
-        <h3 className={timer ? "" : "boundingElem"}>available for Freelance</h3>
+        <a
+          href="https://www.upwork.com/services/product/development-it-website-development-as-full-stack-developer-and-design-website-1860678120887608063?ref=fl_profile"
+          target="blank"
+          className={timer ? "" : "boundingElem"}
+        >
+          available for Freelance
+        </a>
       </div>
       <div className="bounding">
         <h3 className={timer ? "" : "boundingElem"}>Work</h3>
